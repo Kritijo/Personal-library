15f6cfd94a04de961f2a1d4eb73f8fbf1d2d0544
KRITI JOSHI <kritijoshi@KRITIs-MacBook-Pro.local>
2024-10-23 18:02:40 +0530

    Add event listener to display dialog box

---
+++ index.js
@@ -0,0 +1,11 @@
+const dialog = document.querySelector("dialog");
+const newBook = document.querySelector(".bttn-div button");
+const form = document.querySelector("form");
+const bookSection = document.querySelector(".book-section");
+
+const myLibrary = [];
+
+newBook.addEventListener("click",()=>dialog.showModal());
+
+
+

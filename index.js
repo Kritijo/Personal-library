f4cb347dfca6da7200640bb5488d5e0ef9fec070
KRITI JOSHI <kritijoshi@KRITIs-MacBook-Pro.local>
2024-10-23 18:10:49 +0530

    Add event listener on form to capture form details and close dialog box

--- index.js
+++ index.js
@@ -7,5 +7,21 @@ const myLibrary = [];
 
 newBook.addEventListener("click",()=>dialog.showModal());
 
+form.addEventListener("submit",(event)=>{
+    event.preventDefault();
+    let data = new FormData(form);
+    
+    let bookInstance = {};
+    data.forEach((val,key) => bookInstance[key]=val);
+
+    myLibrary.push(bookInstance);
+    console.log(myLibrary);
+    addBookToLibrary();
+
+    dialog.close();
+    form.reset();
+});
+
+
 
 

19d4a9a2332721b7082e6a53121725de37496dfb
KRITI JOSHI <kritijoshi@KRITIs-MacBook-Pro.local>
2024-11-02 16:05:08 +0530

    Refactor addBookToLibrary to use class instead of plain constructor

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ form.addEventListener("submit",(event)=>{
     data.forEach((val,key) => bookInstance[key]=val);
 
     myLibrary.push(bookInstance);
-    addBookToLibrary();
+    new addBookToLibrary().createCards();
 
     dialog.close();
     form.reset();
@@ -33,47 +33,46 @@ class Book{
     }
 }
 
-function addBookToLibrary() {
-    let bookObj = myLibrary[myLibrary.length-1]
-    let book = new Book(bookObj.book, bookObj.author, bookObj.pages, bookObj.status);
+class addBookToLibrary{
+    bookObj = myLibrary[myLibrary.length-1]
+    book = new Book(this.bookObj.book, this.bookObj.author, this.bookObj.pages, this.bookObj.status);
 
-    createCards(book);
-}
-
-function createCards(book){
-    let card = document.createElement("div");
-    card.className = "card"
+    createCards(){
+        let card = document.createElement("div");
+        card.className = "card"
     
-    let cardText1 = document.createElement("div");
-    let cardText2 = document.createElement("div");
-    let cardText3 = document.createElement("div");
-    let cardText4 = document.createElement("div");
-    let cardText5 = document.createElement("div");
-    let deleteCard = document.createElement("button");
-    let changeStatus = document.createElement("button");
-    cardText5.className = "delete-div";
+        let cardText1 = document.createElement("div");
+        let cardText2 = document.createElement("div");
+        let cardText3 = document.createElement("div");
+        let cardText4 = document.createElement("div");
+        let cardText5 = document.createElement("div");
+        let deleteCard = document.createElement("button");
+        let changeStatus = document.createElement("button");
+        cardText5.className = "delete-div";
 
-    cardText1.textContent = `Book : ${book.bookName}`;
-    cardText2.textContent = `Author : ${book.author}`;
-    cardText3.textContent = `Pages : ${book.pages}`;
-    cardText4.textContent = `Status : ${book.status}`;
+        cardText1.textContent = `Book : ${this.book.bookName}`;
+        cardText2.textContent = `Author : ${this.book.author}`;
+        cardText3.textContent = `Pages : ${this.book.pages}`;
+        cardText4.textContent = `Status : ${this.book.status}`;
 
-    changeStatus.textContent = "Change Status"
-    changeStatus.addEventListener("click",()=>{
-        book.status = (book.status === "read") ? "not read yet" : "read";
-        cardText4.textContent = `Status : ${book.status}`;
-    })
+        changeStatus.textContent = "Change Status";
+        deleteCard.textContent = "Delete Card";
 
-    deleteCard.textContent = "Delete Card";
-    deleteCard.addEventListener("click",()=>{
-        let idx = myLibrary.indexOf(book);
-        myLibrary.splice(idx-1,1);
-        card.remove()
-    });
+        cardText5.append(changeStatus, deleteCard);
+        card.append(cardText1,cardText2,cardText3,cardText4,cardText5);
+        bookSection.append(card);
+        this.updateCards(changeStatus,deleteCard,cardText4,card);
+        }
 
-    cardText5.append(changeStatus, deleteCard);
-    card.append(cardText1,cardText2,cardText3,cardText4,cardText5);
-    bookSection.append(card);
+    updateCards(changeStatus,deleteCard,cardText4,card){
+        changeStatus.addEventListener("click",()=>{
+            this.book.status = (this.book.status === "read") ? "not read yet" : "read";
+            cardText4.textContent = `Status : ${this.book.status}`;
+        })
+        deleteCard.addEventListener("click",()=>{
+            let idx = myLibrary.indexOf(this.book);
+            myLibrary.splice(idx-1,1);
+            card.remove()
+        });
+    }
 }
-
-
